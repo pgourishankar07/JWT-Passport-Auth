0be84c947622644b5abfcd6962f23ac97f675726
Gouri Shankar <gourishankar@Gouris-MacBook-Air.local>
2024-08-18 08:46:42 +0530

    sent data from client to server using axios

--- connection/client/src/App.jsx
+++ connection/client/src/App.jsx
@@ -1,15 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   function callAPI() {
     fetch("http://localhost:9000/")
       .then((res) => res.json())
-      .then((data) => setName(data.user));
+      .then((i) => {
+        console.log(i[0].user);
+        setName(i[0].user);
+      });
+  }
+
+  function postAPI() {
+    axios.post("http://localhost:9000/data", data).then((res) => {
+      console.log(res.data);
+    });
+  }
+
+  function handleChange(e) {
+    e.preventDefault();
+    let newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(data);
   }
 
   const [name, setName] = useState("gouri");
+  const [data, setData] = useState({ user: "" });
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +38,10 @@ function App() {
         </p>
         <button className="App-link" onClick={() => callAPI()}>
           <h1>Name :{name}</h1>
+        </button>
+        <input id="user" value={data.user} onChange={(e) => handleChange(e)} />
+        <button type="button" onClick={() => postAPI()}>
+          Push
         </button>
       </header>
     </div>
--- connection/server.js
+++ connection/server.js
@@ -3,15 +3,25 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true })); // get form data
+app.use(express.json()); // parse the json data
+
+const data = [{ user: "bunty" }];
 
 app.get("/", (req, res) => {
-  res.json({ user: "bunty", age: 27 });
+  res.json(data);
 });
 app.get("/contact", (req, res) => {
   res.json({ email: "bunty@gmail.com", phNo: 271324234 });
 });
 app.get("/cart", (req, res) => {
   res.json({ shirt: "white", pant: "blue" });
+});
+
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  data.push({ user: req.body.user });
+  res.redirect("/");
 });
 
 app.listen(9000);
